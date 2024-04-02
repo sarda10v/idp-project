f86bdf159fde9c180be2ad9f98407d9b14f2c6d1
sardalov <sarda10v@bk.ru>
2024-04-02 17:00:08 +0300

    init commit

---
+++ src/Input.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import type { ChangeEvent } from "react";
+import { TextInput } from "@admiral-ds/react-ui";
+import type { TextInputProps, BorderRadiusType } from "@admiral-ds/react-ui";
+
+export const Input = ({
+  value = "Привет!",
+  placeholder = "Placeholder",
+  themeBorderKind,
+  CSSCustomProps,
+  ...props
+}: TextInputProps & {
+  themeBorderKind?: BorderRadiusType;
+  CSSCustomProps?: boolean;
+}) => {
+  const [localValue, setValue] = React.useState<string>(String(value) ?? "");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    setValue(inputValue);
+    props.onChange?.(e);
+  };
+
+  return (
+    <TextInput
+      {...props}
+      value={localValue}
+      placeholder={placeholder}
+      onChange={handleChange}
+      displayClearIcon={true}
+      skeleton={false}
+    />
+  );
+};
