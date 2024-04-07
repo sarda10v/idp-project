fcac5c19a1f5fdf7d9d85a07b56538def22c9589
sardalov <sarda10v@bk.ru>
2024-04-07 18:57:25 +0300

    update type config server

--- src/serverUrl.ts
+++ src/serverUrl.ts
@@ -1,9 +1,9 @@
 import { BASE_URL } from "./features/constants";
-const { DEV } = import.meta.env;
-export let SERVER_URL: any;
+const { DEV, PROD } = import.meta.env;
+export let SERVER_URL: string;
 
 if (DEV) {
   SERVER_URL = BASE_URL; // адрес сервера на локалке
-} else {
+} else if (PROD) {
   SERVER_URL = "https://idp-project-server.onrender.com"; // адрес сервера после выгрузки
 }
