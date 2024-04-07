0e82dd0d8937bb7d048a64b52949673a6b8d60a5
sardalov <sarda10v@bk.ru>
2024-04-07 18:25:09 +0300

    fix config server

--- src/serverUrl.ts
+++ src/serverUrl.ts
@@ -1,9 +1,8 @@
 import { BASE_URL } from "./features/constants";
-const { NODE_ENV } = process.env;
-
+const { DEV } = import.meta.env;
 export let SERVER_URL: any;
 
-if (NODE_ENV === "development") {
+if (DEV) {
   SERVER_URL = BASE_URL; // адрес сервера на локалке
 } else {
   SERVER_URL = "https://idp-project-server.onrender.com"; // адрес сервера после выгрузки
